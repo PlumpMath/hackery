556abd7bc8b05ed1320ec025b0457f18ff38f131
Brian Rowe <browe@rentpath.com>
2014-08-14 22:10:30 -0400

    can now mapcat organizations into the org-chan

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+                 [cheshire "5.3.1"]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
+                 [http-kit "2.1.16"]])
--- src/hackery/core.clj
+++ src/hackery/core.clj
@@ -1,6 +1,33 @@
-(ns hackery.core)
+(ns hackery.core
+  (:require [org.httpkit.client :as http]
+            [cheshire.core :as json]
+            [clojure.core.async :as async
+             :refer [<! >! <!! timeout chan alt! go
+                     go-loop alts! close! put!]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:const token (slurp "token"))
+(def ^:const host "https://api.github.com")
+
+(defn req
+  ([url]
+     @(http/get
+       (str host url)
+       {:headers {"Authorization" (format "token %s" token)
+                  "Accept" "application/vnd.github.v3+json"}}))
+  ([url chan]
+     (http/get
+      (str host url)
+      {:headers {"Authorization" (format "token %s" token)
+                 "Accept" "application/vnd.github.v3+json"}}
+      #(put! chan %))))
+
+(defn read-orgs
+  [{:keys [body] :as resp}]
+  (json/parse-string body true))
+
+(defn mapcatting [f]
+  (fn [f1]
+    (fn [result input]
+      (reduce f1 result (f input)))))
+
+(def orgs-chan (chan 1 (mapcatting read-orgs)))
